4826f1c3809e838d49bb34da0f485ed51b67b52e
Teresa Keller <56941889+tkeller333@users.noreply.github.com>
2021-07-28 12:02:55 -0500

    added teacher class

---
+++ SchoolPractice/Teacher.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SchoolPractice
+{
+    class Teacher
+    {
+        private string FirstName { get; set; }
+        private string LastName { get; set; }
+        private string Subject { get; set; }
+        private int YearsTeaching { get; set; }
+
+        public Teacher(string firstName, string lastName, string subject, int yearsTeaching)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Subject = subject;
+            YearsTeaching = yearsTeaching;
+        }
+    }
+}
