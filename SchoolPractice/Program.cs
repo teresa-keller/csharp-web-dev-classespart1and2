96878dbb6d3cca56ccdcd91c0c90d4d551f67a82
Teresa Keller <56941889+tkeller333@users.noreply.github.com>
2021-07-27 20:27:53 -0500

    started chapter 4 exercises

--- SchoolPractice/Program.cs
+++ SchoolPractice/Program.cs
@@ -7,6 +7,11 @@ namespace SchoolPractice
         public static void Main(string[] args)
         {
             // TODO: Instantiate your objects and test your exercise solutions with print statements here.
+            Student myStudent = new Student("Teresa", 666, 1, 4.0);
+            Console.WriteLine("Student name: " + myStudent.Name);
+            Console.WriteLine("Student ID: " + myStudent.StudentId);
+            Console.WriteLine("Number of credits: " + myStudent.NumberOfCredits);
+            Console.WriteLine("GPA: " + myStudent.Gpa);
         }
     }
 }
